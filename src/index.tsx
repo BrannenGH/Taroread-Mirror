895ca1e0492350e048b13124751718807fb9987f
Brannen Hall <Brannen@HallB.me>
2021-05-23 02:13:54 +0000

    Add in Taroread theming

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import Logo from './logo.svg';
 import './index.css';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-import {AppBar, Toolbar, Card, Typography, CardContent, Container, Grid, Box, CardMedia, CardActionArea } from "@material-ui/core";
+import {AppBar, Toolbar, Card, Typography, CardContent, Container, Grid, Box, CardMedia, CardActionArea, ThemeProvider } from "@material-ui/core";
 import { spacing } from '@material-ui/system';
 import { Learn } from './learn/learn'
 import {
@@ -12,11 +12,24 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import { createMuiTheme } from '@material-ui/core/styles';
 
 function App() {
+  const taroreadTheme = createMuiTheme({
+    palette: {
+      primary: {
+        main: '#341849'
+      },
+      secondary: {
+        main: '#0C466C'
+      }
+    }
+  })
+
+
   return (
-    <Router>
-      <div>
+    <ThemeProvider theme={taroreadTheme}>
+      <Router>
         <AppBar position="static">
           <Toolbar>
             <img src={Logo} alt="Taroread" className="logo"></img> 
@@ -27,8 +40,8 @@ function App() {
         <Box m={3}>
           <Learn></Learn>
         </Box>
-      </div>
-    </Router>
+      </Router>
+    </ThemeProvider>
   );
 }
 
