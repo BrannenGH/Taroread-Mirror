f91ad171ef6a5a9a37b79d6b8268bccd14f26018
Brannen Hall <Brannen@HallB.me>
2021-06-11 16:16:50 +0000

    Add styling improvements to JournalEdit

--- src/journal/journal-edit/journal-edit.tsx
+++ src/journal/journal-edit/journal-edit.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {AppBar, TextField, Toolbar, Card, Typography, CardContent, Container, Grid, Box, CardMedia, CardActionArea, Button } from "@material-ui/core";
+import {AppBar, Paper, TextField, Toolbar, Card, Typography, CardContent, Container, Grid, Box, CardMedia, CardActionArea, Button } from "@material-ui/core";
 import {
   BrowserRouter as Router,
   Switch,
@@ -23,82 +23,98 @@ const JournalEdit = (props: any) => {
     const journal = props.allJournals[id];
 
     return (
-        <Formik
-            initialValues={journal}
-            onSubmit={(values, { setSubmitting }) => {
-                const journals = props.allJournals;
-                journals[id] = values;
-                props.onModify(journals)
-                setSubmitting(false)
-            }}>
-                {({
-                    values,
-                    errors,
-                    touched,
-                    handleChange,
-                    handleBlur,
-                    handleSubmit,
-                    isSubmitting,
-                }) => (
-                    <form onSubmit={handleSubmit}>
-                        <Grid
-                            direction="column">
-                            <Grid item>
-                                <TextField
-                                    name="title"
-                                    required
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    defaultValue={values.title} />
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    name="date"
-                                    type="date"
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    defaultValue={values.date} />
-                            </Grid>
-                            <Grid item>
-                                <FieldArray
-                                    name="cards"
-                                    render={arrayHelpers => (
-                                        <Grid container>
-                                        {
-                                            values?.cards?.map((card: any, i: number) => (
-                                                <Grid item md={1}>
-                                                    <TarotCardField
-                                                        name={`cards.${i}`}
-                                                        onChange={handleChange}
-                                                        onBlur={handleBlur}
-                                                        onDelete={() => arrayHelpers.remove(i)}
-                                                        initialValue={card}
-                                                        allCards={props.allCards} />
-                                                </Grid>
-                                            ))
-                                        }
-                                            <Grid item md={1}>
-                                                <Add
-                                                    onClick={() => arrayHelpers.insert(values?.cards?.length ?? 0, 1)} />
+        <Grid container alignItems="center" justify="center">
+            <Grid item xs = {false} md={2}>
+            </Grid>
+            <Grid item xs={12} md={8}>
+                <Paper>
+                    <Formik
+                        initialValues={journal}
+                        onSubmit={(values, { setSubmitting }) => {
+                            const journals = props.allJournals;
+                            journals[id] = values;
+                            props.onModify(journals)
+                            setSubmitting(false)
+                        }}>
+                            {({
+                                values,
+                                errors,
+                                touched,
+                                handleChange,
+                                handleBlur,
+                                handleSubmit,
+                                isSubmitting,
+                            }) => (
+                                <Box m={2}>
+                                    <form onSubmit={handleSubmit}>
+                                        <Grid
+                                            direction="column">
+                                            <Grid item>
+                                                <TextField
+                                                    name="title"
+                                                    label="Title"
+                                                    required
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    defaultValue={values.title} />
+                                            </Grid>
+                                            <Grid item>
+                                                <TextField
+                                                    name="date"
+                                                    label="Date"
+                                                    type="date"
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    defaultValue={values.date} />
+                                            </Grid>
+                                            <Grid item>
+                                                <FieldArray
+                                                    name="cards"
+                                                    render={arrayHelpers => (
+                                                        <Grid container>
+                                                        {
+                                                            values?.cards?.map((card: any, i: number) => (
+                                                                <Grid item xs={6} md={1}>
+                                                                    <TarotCardField
+                                                                        name={`cards.${i}`}
+                                                                        onChange={handleChange}
+                                                                        onBlur={handleBlur}
+                                                                        onDelete={() => arrayHelpers.remove(i)}
+                                                                        initialValue={card}
+                                                                        allCards={props.allCards} />
+                                                                </Grid>
+                                                            ))
+                                                        }
+                                                            <Grid item md={1}>
+                                                                <Add
+                                                                    onClick={() => arrayHelpers.insert(values?.cards?.length ?? 0, 1)} />
+                                                            </Grid>
+                                                        </Grid>
+                                                    )}/>
+                                            </Grid>
+                                            <Grid item>
+                                                <TextField
+                                                    name="description"
+                                                    label="Notes"
+                                                    multiline
+                                                    onChange={handleChange}
+                                                    onBlur={handleBlur}
+                                                    defaultValue={values.description} />
+                                            </Grid>
+                                            <Grid item>
+                                                <Button type="submit" disabled={isSubmitting}>Save</Button>
                                             </Grid>
                                         </Grid>
-                                    )}/>
-                            </Grid>
-                            <Grid item>
-                                <TextField
-                                    name="description"
-                                    multiline
-                                    onChange={handleChange}
-                                    onBlur={handleBlur}
-                                    defaultValue={values.description} />
-                            </Grid>
-                            <Grid item>
-                                <Button type="submit" disabled={isSubmitting}>Save</Button>
-                            </Grid>
-                        </Grid>
-                    </form>
-                )}
-        </Formik>
+                                    </form>
+                                </Box>
+                            )}
+                    </Formik>
+                </Paper>
+            </Grid>
+            <Grid item xs = {false} md={2}>
+
+            </Grid>
+        </Grid>
     )
 }
 
