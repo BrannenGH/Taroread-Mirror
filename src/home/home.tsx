e897c9a7f412160bddf9ee58148dc12c75595da9
Brannen Hall <Brannen@HallB.me>
2021-06-12 17:06:25 +0000

    Add helper cards to home screen

--- src/home/home.tsx
+++ src/home/home.tsx
@@ -1,7 +1,33 @@
-import { Typography } from "@material-ui/core";
+import React from "react";
+import { Typography, Grid, Card, Box } from "@material-ui/core";
+import { Link } from "react-router-dom";
 
 const Home = (props: any) => {
-  return <Typography>Welcome to Taroread!</Typography>;
+  return (
+    <Box>
+      <Typography>Welcome to Taroread!</Typography>
+      <Grid container>
+        <Grid item md={4} xs={6}>
+          <Link to="/learn">
+            <Card>
+              <Typography>
+                Learn about what each of the Tarot Cards mean
+              </Typography>
+            </Card>
+          </Link>
+        </Grid>
+        <Grid item md={4} xs={6}>
+          <Link to="/journal">
+            <Card>
+              <Typography>
+                Record your readings to track your progress
+              </Typography>
+            </Card>
+          </Link>
+        </Grid>
+      </Grid>
+    </Box>
+  );
 };
 
 export { Home };
