2743d29e1bbd450c8ba681a46c5a12c8f0ae2c4f
Brannen Hall <Brannen@HallB.me>
2021-06-12 15:58:42 +0000

    Allow for over 100 tarot cards to be used

--- scripts/fetch-metadata.js
+++ scripts/fetch-metadata.js
@@ -3,10 +3,37 @@ const fs = require('fs');
 
 const apiBase = 'https://api.hallb.me';
 
+const getTarotMetadataFromApi = () => {
+    const getMetadataAtStart = (start) => {
+      return axios
+        .get(apiBase + `/tarot-cards?_start=${start}`)
+        .then(res => res.data)
+    }
+    
+    return (
+      axios
+        .get(apiBase + '/tarot-cards/count')
+        .then(res => {
+          const count = res.data;
+          const promises = [];
+          for(var i = 1; i < count; i += 100) {
+            promises.push(getMetadataAtStart(i));
+          }
+  
+          return Promise.all(promises);
+        })
+        // Sort cards by their value attribute
+        .then((res) =>
+            res.flat().sort(function (a, b) {
+              return a.value - b.value;
+            })
+          )
+      );
+  }
+
 const fetchMetadata = () => {
-    return axios.get(apiBase + '/tarot-cards')
-    // Sort cards by their value attribute
-        .then(res => fs.writeFileSync('src/tarot-metadata.json', JSON.stringify(res.data, null, 4)))
+    return getTarotMetadataFromApi()
+        .then(res => fs.writeFileSync('src/tarot-metadata.json', JSON.stringify(res, null, 4)))
 }
 
 fetchMetadata();
--- src/shared/tarot-cards/tarot-card-service.ts
+++ src/shared/tarot-cards/tarot-card-service.ts
@@ -4,18 +4,37 @@ import tarotcardMetadata from "../../tarot-metadata.json";
 
 const apiBase = "https://api.hallb.me";
 
-const getTarotMetadata: () => Promise<TarotCardMetadata[]> = () => {
-  if (!tarotcardMetadata) {
-    return (
-      axios
-        .get(apiBase + "/tarot-cards")
-        // Sort cards by their value attribute
-        .then((res) =>
-          res.data.sort(function (a: any, b: any) {
+const getTarotMetadataFromApi = () => {
+  const getMetadataAtStart = (start: number) => {
+    return axios
+      .get(apiBase + `/tarot-cards?_start=${start}`)
+      .then(res => res.data)
+  }
+  
+  return (
+    axios
+      .get(apiBase + '/tarot-cards/count')
+      .then(res => {
+        const count = res.data;
+        const promises = [];
+        for(var i = 1; i < count; i += 100) {
+          promises.push(getMetadataAtStart(i));
+        }
+
+        return Promise.all(promises);
+      })
+      // Sort cards by their value attribute
+      .then((res) =>
+          res.flat().sort(function (a, b) {
             return a.value - b.value;
           })
         )
     );
+}
+
+const getTarotMetadata: () => Promise<TarotCardMetadata[]> = () => {
+  if (!tarotcardMetadata) {
+    return getTarotMetadataFromApi();
   } else {
     return new Promise<TarotCardMetadata[]>((resolve) =>
       resolve(tarotcardMetadata as unknown as TarotCardMetadata[])
