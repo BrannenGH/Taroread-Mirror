5e94da151fe7b61ac7ede3a33c64eadf311c9e80
Brannen Hall <brannen@hallb.me>
2021-09-02 12:59:19 +0000

    Merged PR 6: Update Android build process to better support versioning
    - Update app id and add azure build plugin to update Android version in manifest
    - Update manifest and build files
    - Add user friendly display names for build tasks
    
    Related work items: #94

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'me.tarotantula.taroread',
   appName: 'Taroread',
   webDir: 'build',
   bundledWebRuntime: false
