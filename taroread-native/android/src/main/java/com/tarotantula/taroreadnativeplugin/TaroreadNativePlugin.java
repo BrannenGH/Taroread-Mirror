895bd6fe99f217b97f20bbbf54f98f6187e9215e
Brannen Hall <Brannen@HallB.me>
2021-09-24 14:04:12 -0500

    Pass Java exceptions back to JavaScript intepreter

--- taroread-native/android/src/main/java/com/tarotantula/taroreadnativeplugin/TaroreadNativePlugin.java
+++ taroread-native/android/src/main/java/com/tarotantula/taroreadnativeplugin/TaroreadNativePlugin.java
@@ -15,42 +15,58 @@ public class TaroreadNativePlugin extends Plugin {
 
     @PluginMethod()
     public void signInWithGoogle(PluginCall call) {
-        Intent firebaseIntent = new Intent(getContext(), FirebaseBActivity.class);
-        startActivityForResult(call, firebaseIntent, "signInWithGoogleResult");
+        try {
+            Intent firebaseIntent = new Intent(getContext(), FirebaseBActivity.class);
+            startActivityForResult(call, firebaseIntent, "signInWithGoogleResult");
+        } catch (Exception e) {
+            call.reject("Couldn't complete google sign-in intent", e);
+        }
     }
 
     @PluginMethod()
     public void signOut(PluginCall call) {
-        FirebaseAuth.getInstance().signOut();
-        call.resolve();
+        try {
+            FirebaseAuth.getInstance().signOut();
+            call.resolve();
+        } catch (Exception e) {
+            call.reject("Couldn't sign-out", e);
+        }
     }
 
     @PluginMethod()
     public void getUser(PluginCall call) {
-        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        try {
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
-        JSObject response = new JSObject();
-        if (user != null) {
-            response.put("displayName", user.getDisplayName());
-            response.put("email", user.getEmail());
-            response.put("photoURL", user.getPhotoUrl());
-            call.resolve(response);
-        } else {
-            call.resolve(null);
+            JSObject response = new JSObject();
+            if (user != null) {
+                response.put("displayName", user.getDisplayName());
+                response.put("email", user.getEmail());
+                response.put("photoURL", user.getPhotoUrl());
+                call.resolve(response);
+            } else {
+                call.resolve(null);
+            }
+        } catch (Exception e) {
+            call.reject("Couldn't get the current user", e);
         }
     }
 
     @ActivityCallback()
     private void signInWithGoogleResult(PluginCall call, ActivityResult result) {
-        Intent resultIntent = result.getData();
-        String displayName = resultIntent.getStringExtra("displayName");
-        String email = resultIntent.getStringExtra("email");
-        String photoUrl = resultIntent.getStringExtra("photoUrl");
+        try {
+            Intent resultIntent = result.getData();
+            String displayName = resultIntent.getStringExtra("displayName");
+            String email = resultIntent.getStringExtra("email");
+            String photoUrl = resultIntent.getStringExtra("photoUrl");
 
-        JSObject response = new JSObject();
-        response.put("displayName", displayName);
-        response.put("email", email);
-        response.put("photoURL", photoUrl);
-        call.resolve(response);
+            JSObject response = new JSObject();
+            response.put("displayName", displayName);
+            response.put("email", email);
+            response.put("photoURL", photoUrl);
+            call.resolve(response);
+        } catch (Exception e) {
+            call.reject("Couldn't handle result of sign-in intent", e);
+        }
     }
 }
